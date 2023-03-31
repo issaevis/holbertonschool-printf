7607a5c25e9c7c3af6842049b200722ef40a9658
akka97 <akapellari@yahoo.com>
2023-03-31 01:08:06 -0700

    fixed _putchar

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
-* _putchar - writes the character c to stdout
+* _putchar - Writes the character c to stdout
 * @c: The char
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
